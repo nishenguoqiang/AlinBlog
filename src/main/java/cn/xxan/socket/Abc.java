74bcf00cbc4b171eacf5ec3bf521e1574d957e72
nishenguoqiang <shenguoqiang@navinfo.com>
2018-03-29 10:17:33 +0800

    wordCountMR

---
+++ src/main/java/cn/xxan/socket/Abc.java
@@ -0,0 +1,26 @@
+package cn.xxan.socket;
+
+import cn.xxan.reflect.User;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class Abc {
+
+    public static void main(String[] args){
+        User zhangsan = new User("zhangsan", 123);
+
+//        HashMap<String, String> map = new HashMap<>();
+//        TreeMap<String, String> map = new TreeMap<>();
+        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+        map.put("name","zhangsan");
+        map.put("age","19");
+        map.put("adresss","xian");
+
+        String key = map.entrySet().iterator().next().getKey();
+        String value = map.entrySet().iterator().next().getValue();
+        System.out.println(key + "," + value);
+
+    }
+}
--- src/test/java/cn/xxan/AlinTest.java
+++ src/test/java/cn/xxan/AlinTest.java
@@ -1,7 +1,9 @@
 package cn.xxan;
 
+import org.apache.commons.dbutils.QueryRunner;
 import org.junit.Test;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -21,6 +23,33 @@ public class AlinTest {
             System.out.println(iterator.next());
         }
 
+    }
 
+    @Test
+    public void saveNum() throws Exception{
+        String driver = "oracle.jdbc.OracleDriver";    //驱动标识符
+        String url ="jdbc:oracle:thin:@192.168.3.67:1521:orcl";  // 连接远程的数据库可以这么写
+        String user = "nirobot_280_shenguoqiang";         //数据库的用户名
+        String password = "nirobot_280_shenguoqiang";     //数据库的密码
+        Connection con = null;
+        PreparedStatement pstm = null;
+        ResultSet rs = null;
+        boolean flag = false;
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+            String sql = "insert into rd_link_temp select * from rd_link";
+            String sql0 = "insert into update_date values(sysdate)";
+            QueryRunner runner = new QueryRunner();
+            con.setAutoCommit(false);
+            runner.update(con,sql);
+//            int abc = 10/0;
+            con.commit();
+            runner.update(con,sql0);
+            con.commit();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
